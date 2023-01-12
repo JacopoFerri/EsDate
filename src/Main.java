313a8b876139a219bbc6864f05ec0b8e0ea67619
DESKTOP-TAAKBNQ\trian2 <jacopoferri97@gmail.com>
2023-01-12 12:39:31 +0100

    EsDate

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Util data = new Util();
+		for (int mese = 1; mese<= 12; mese++ ) {
+			for (int giorno = 1; giorno<= 31; giorno++) {
+				try {
+					LocalDate date = LocalDate.of(2023, mese, giorno);
+					System.out.println("Data attuale: " + date);
+					System.out.println(data.generateInsertDate(5,date));
+				} catch(Exception e) {
+					continue;	
+				}
+			}
+		}
+	}
+
+}
---
+++ src/Util.java
@@ -0,0 +1,27 @@
+ import java.time.LocalDate;
+
+public class Util {
+	private int year;
+	private int month;
+	private int day = 1;
+	//private LocalDate date = LocalDate.now();
+	
+	public String generateInsertDate(int Nr, LocalDate date) {
+		if (Nr<0 || Nr > 12) {
+			throw new RuntimeException("Nr deve essere compreso tra 1 e 12");
+		}
+		if(Nr == date.getMonthValue()) {
+			year = date.getYear()-1;
+			month = 12;
+		} else if(Nr > date.getMonthValue()) {
+			year = date.getYear()-1;
+			month = 12-(Nr - date.getMonthValue());
+		} else {
+			year = date.getYear();
+			month = date.getMonthValue()-Nr;
+		}
+		LocalDate dateQuery = LocalDate.of(year, month, day);
+		return dateQuery.toString();
+	}
+}
+
