3101b065369f31f9164c9b8689abb8ddabe40cb2
Jacopo <jacopoferri97@gmail.com>
2023-01-24 15:26:04 +0100

    Aggiunte le altre prove

--- src/Util.java
+++ src/Util.java
@@ -6,8 +6,10 @@ public class Util {
 	private int month;
 	private int day = 1;
 	//private LocalDate date = LocalDate.now();
-	
 	public String generateInsertDate(int Nr, LocalDate date) {
+		return date.with(TemporalAdjusters.firstDayOfMonth()).minusMonths(Nr).toString();
+	}
+	public String generateInsertDate_bis(int Nr, LocalDate date) {
 		if (Nr<0 || Nr > 12) {
 			throw new RuntimeException("Nr deve essere compreso tra 1 e 12");
 		}
@@ -24,8 +26,22 @@ public class Util {
 		LocalDate dateQuery = LocalDate.of(year, month, day);
 		return dateQuery.toString();
 	}
-	public String generateInsertDate_bis(int Nr, LocalDate date) {
-		return date.with(TemporalAdjusters.firstDayOfMonth()).minusMonths(Nr).toString();
+	public static String generateInsertDate1 (int csvReportPreviousMonthNr, LocalDate date) {
+		String dataInserimento = null;
+		int yearPrec = date.getYear() - 1;
+		int monthPrec = date.getMonthValue() - csvReportPreviousMonthNr;
+		int addMonth = monthPrec + 12;
+
+		if (monthPrec > 0 && monthPrec <= 9) {
+			dataInserimento = date.getYear() + "-0" + monthPrec + "-01";
+		} else if (monthPrec > 9) {
+			dataInserimento = date.getYear() + "-" + monthPrec + "-01";
+		} else if (monthPrec <= 0 && monthPrec > -3) {
+			dataInserimento = yearPrec + "-" + addMonth + "-01";
+		} else if (monthPrec <=-3) {
+			dataInserimento = yearPrec + "-0" + addMonth + "-01";
+		}
+		return dataInserimento;
 	}
 }
 
